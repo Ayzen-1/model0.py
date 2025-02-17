0617231cf2443d0d6554ab7c84994c6e71490caf
Ayzen-1 <139116623+Ayzen-1@users.noreply.github.com>
2025-02-17 18:50:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Я что то делаю')
 
-st.write('Саламалейкум!')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+  st.write('X')   
+  X_raw = df.drop('species', axis=1)
+  st.dataframe(X_raw)
+  
+  st.write('y')
+  y_raw = df.species
+  st.dataframe(y_raw)
